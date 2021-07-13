dc434caa9fb6477753141aaa7a3c1d0c73d17904
Sercan Evyapan <sercanevyapan@outlook.com>
2021-07-13 16:21:54 +0300

    Add api architecture

--- Angular_Ecommerce/Controllers/ProductsController.cs
+++ Angular_Ecommerce/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
-﻿using API.Data.DataContext;
-using API.Data.DbModels;
+﻿
+using API.Core.DbModels;
+using API.Infrastructure.DataContext;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
